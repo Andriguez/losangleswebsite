27b6cc0d9ea8bea1e1f90cefea33398eaa22f523
Andy Rodriguez <577147@student.inholland.nl>
2023-11-30 20:28:22 +0100

    modified the given DB configuration file

--- Webdev1_EndAssignment_IT2B_577147/app/config/db.php
+++ Webdev1_EndAssignment_IT2B_577147/app/config/db.php
@@ -4,17 +4,21 @@ class DB extends PDO
 {
     private static DB $instance;
 
-    public static function getInstance():DB
+    private function __construct($dsn, $user, $password){
+        parent::__construct($dsn, $user,$password);
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+    }
+    public static function getInstance(array $config): DB
     {
         if (empty(self::$instance)) {
             try {
-                $dbOptions = self::getConfig();
+                //$dbOptions = self::getConfig();
 
-                $port = $dbOptions['port'];
-                $host = $dbOptions['hostname'];
-                $name = $dbOptions['name'];
-                $user = $dbOptions['username'];
-                $password = $dbOptions['password'];
+                $port = $config['port'];
+                $host = $config['hostname'];
+                $name = $config['name'];
+                $user = $config['username'];
+                $password = $config['password'];
                 $charset = 'utf8mb4';
 
                 $dsn = "mysql:host=$host;port=$port;dbname=$name;charset=$charset";
@@ -29,15 +33,33 @@ class DB extends PDO
     }
 
 
-    static function getConfig()
+    static function getConfigs():array
     {
-        return [
+        return $configs = [
+        $config1 = [
 
-                'hostname' => $_ENV['PHP_DB_HOST'],
+                'hostname' => $_ENV['mysql'],
                 'port' => 3306,
-                'username' => $_ENV['MYSQL_USER'],
-                'password' => $_ENV['MYSQL_ROOT_PASSWORD'],
-                'name' => $_ENV['MYSQL_DATABASE']
+                'username' => $_ENV['developer'],
+                'password' => $_ENV['secret123'],
+                'name' => $_ENV['losangles_content']
+        ],
+        $config2 = [
+
+            'hostname' => $_ENV['mysql'],
+            'port' => 3306,
+            'username' => $_ENV['developer'],
+            'password' => $_ENV['secret123'],
+            'name' => $_ENV['losangles_users']
+        ],
+        $config3 = [
+
+            'hostname' => $_ENV['mysql'],
+            'port' => 3306,
+            'username' => $_ENV['developer'],
+            'password' => $_ENV['secret123'],
+            'name' => $_ENV['losangles_feed']
+        ]
         ];
     }
 }
