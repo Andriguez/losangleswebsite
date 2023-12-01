7e3a6138d5b8b522daa60912356f40f323a2a357
Andy Rodriguez <577147@student.inholland.nl>
2023-12-01 02:33:29 +0100

    made the first repository

--- Webdev1_EndAssignment_IT2B_577147/app/repositories/UserTypeRepository.php
+++ Webdev1_EndAssignment_IT2B_577147/app/repositories/UserTypeRepository.php
@@ -4,10 +4,33 @@ require_once __DIR__.'/../models/UserType.php';
 class UserTypeRepository extends Repository
 {
     public function getUserTypeById($id){
-        try{
+        $query = "SELECT usertype_Id, usertype_name FROM users_usertypes WHERE usertype_Id = :id";
 
-        } catch (\PDOException $e){
-            echo $e;
-        }
+        try{
+            $statement = $this->users_db->prepare($query);
+            $statement->bindParam(':id', $id);
+            $statement->execute();
+
+            $statement->setFetchMode(PDO::FETCH_CLASS, 'UserType');
+            return $statement->fetch();
+            
+        } catch (\PDOException $e){echo $e;}
+    }
+
+    public function getAllUserTypes(){
+        $query = "SELECT usertype_Id FROM users_usertypes";
+
+        try{
+            $statement = $this->users_db->prepare($query);
+            $statement->execute();
+
+            while($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+
+                $userType = $this->getUserTypeById($row['usertype_Id']);
+                $allUserTypes[] = $userType;
+            }
+
+            return $allUserTypes;
+        }catch (\PDOException $e){echo $e;}
     }
 }
