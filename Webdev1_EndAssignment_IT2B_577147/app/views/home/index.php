86b0735b27a0b328aebecfcc2a82291169786008
Andy Rodriguez <577147@student.inholland.nl>
2023-12-04 04:30:24 +0100

    hard coded the nav bar

--- Webdev1_EndAssignment_IT2B_577147/app/views/home/index.php
+++ Webdev1_EndAssignment_IT2B_577147/app/views/home/index.php
@@ -1,40 +1,68 @@
 <?php
 
-use models\ContentType;
-use Repositories\Repository;
-
-try{
-    require __DIR__ . '/../../repositories/Repository.php';
-    require __DIR__ . '/../../models/ContentType.php';
-
-
-    $repo = new Repository();
-
-    $stmt = $repo->content_db->prepare('SELECT * FROM content_types');
-    $stmt->execute();
-    $contents = new ArrayObject();
-
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-    $content = new ContentType();
-    $content->setTypeId($row['content_type_Id']);
-    $content->setName($row['content_type_name']);
-
-    $contents->append($content);
-    }
-
-} catch (PDOException $e){
-    echo "Connection failed: ". $e->getMessage();
-}
 ?>
 <html>
 <head>
     <title>Los Angles</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-<h1>Los Angles Home</h1>
-<?php foreach ($contents as $c){
- echo '<p>'.$c->getTypeId().'has name '.$c->getName().'</p>';
-} ?>
+<div id="page" class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between pt-2 pb-3 mb-4">
+
+        <ul class="nav col-12 mb-2 justify-content-center mb-md-0">
+            <li><h3><a href="#" class="nav-link mt-3 ms-2 me-3 ps-2 pe-5">Artists</a></h3></li>
+            <li><h3><a href="#" class="nav-link mt-3 mx-4 px-5">Events</a></h3></li>
+            <div class="mb-2 mb-md-0">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none navbar-brand px-5">
+                    <img src="/media/logocrop.jpeg" alt="Logo" width="125" height="80" class="d-inline-block align-text-top">
+                </a>
+            </div>
+
+            <li><h3><a href="#" class="nav-link mt-3 mx-4 px-5">About</a></h3></li>
+            <li><h3><a href="#" class="nav-link mt-3 ms-3 me-2 ps-4 pe-2" role="button" data-bs-toggle="dropdown" aria-expanded="false">Connect</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Register</a></li>
+                    <li><a class="dropdown-item" href="#">Login</a></li>
+                </ul>
+                </h3>
+            </li>
+        </ul>
+    </header>
+    <!--<img class="logoHomepage" src="/media/onlytb.png" alt="Logo" width="1085" height="540">-->
+</div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 </html>
+
+<style>
+    @page {
+        position: relative;
+        width: 100%;
+        height: 500px; /* Set the height as needed */
+        background-color: #f0f0f0;
+    }
+
+    .logoHomepage {
+        position: absolute;
+        top: -7px; /* Adjust the top position as needed */
+        left: 50px; /* Adjust the left position as needed */
+        z-index: 1; /* Set the z-index value */
+    }
+
+    a:hover{
+        color: white !important; /* New text color on hover */
+        text-shadow:
+                -1px -1px 0 #000,
+                1px -1px 0 #000,
+                -1px 1px 0 #000,
+                1px 1px 0 #000000;
+    }
+    a{
+        color: black !important;
+    }
+</style>
