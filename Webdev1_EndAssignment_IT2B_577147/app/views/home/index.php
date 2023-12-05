55d2f09b215d68a29c70fe5a2acd6dcb71ccc25a
Andy Rodriguez <577147@student.inholland.nl>
2023-12-05 01:56:17 +0100

    navbar + homepage

--- Webdev1_EndAssignment_IT2B_577147/app/views/home/index.php
+++ Webdev1_EndAssignment_IT2B_577147/app/views/home/index.php
@@ -8,36 +8,10 @@
 </head>
 <body>
     <div id="page" class="container px-0">
-        <!--
-        <header class="d-flex flex-wrap align-items-center justify-content-between pt-2 pb-3 mb-4">
-            <ul class="nav col-12 mb-2 justify-content-center mb-md-0">
-                <li><h3><a href="#" class="nav-link mt-3 ms-2 me-3 ps-2 pe-3 pe-md-5">Artists</a></h3></li>
-                <li><h3><a href="#" class="nav-link mt-3 mx-3 mx-md-4 px-3 px-md-5">Events</a></h3></li>
+        <header class="d-flex flex-nowrap align-items-center justify-content-between pt-2 text-secondary">
 
-                <li class="nav-item">
-                    <a href="/" class="d-inline-flex link-body-emphasis ms-5 me-4 text-decoration-none navbar-brand">
-                        <img src="/media/logo_placeholder.png" alt="Logo" width="125" height="80" class="d-inline-block align-text-top">
-                    </a>
-                </li>
-
-                <li><h3><a href="#" class="nav-link mt-3 mx-3 mx-md-4 px-3 px-md-5">About</a></h3></li>
-                <li>
-                    <h3>
-                        <a href="#" class="nav-link mt-3 ms-3 me-2 ps-3 pe-2 ps-md-5 pe-md-2" role="button" data-bs-toggle="dropdown" aria-expanded="false">Connect</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Register</a></li>
-                            <li><a class="dropdown-item" href="#">Login</a></li>
-                        </ul>
-                    </h3>
-                </li>
-            </ul>
-        </header>
--->
-        <header class="d-flex flex-nowrap align-items-center justify-content-between pt-2">
-
-            <div class="col mb-3 mb-md-0">
-                <!-- Navigation Links -->
-                <ul class="nav justify-content-center">
+            <div class="col mb-1 mb-md-0">
+                <ul class="nav justify-content-center text-bg-body">
                     <li class="nav-item"><h3><a href="#" class="nav-link mt-3 mx-5">Artists</a></h3></li>
                     <li class="nav-item"><h3><a href="#" class="nav-link mt-3 mx-5">Events</a></h3></li>
                     <div class="mb-2 mb-md-0 mx-5">
@@ -48,8 +22,7 @@
                     </div>
 
                     <li class="nav-item"><h3><a href="#" class="nav-link mt-3 mx-5">About</a></h3></li>
-                    <li><!-- Connect Dropdown -->
-                        <h3><a class="nav-link mt-3 ms-4 me-5" id="connectDropdown" data-bs-toggle="dropdown" aria-expanded="false">Connect</a>
+                    <li class="nav-item"><h3><a href="#" class="nav-link mt-3 ms-4 me-5" id="connectDropdown" data-bs-toggle="dropdown" aria-expanded="false">Connect</a>
                             <ul class="dropdown-menu" aria-labelledby="connectDropdown">
                                 <li><a class="dropdown-item" href="#">Register</a></li>
                                 <li><a class="dropdown-item" href="#">Login</a></li>
@@ -62,7 +35,7 @@
 
 
 
-        <img class="logoHomepage" src="/media/onlytb.png" alt="Logo" width="1085" height="400">
+        <img class="logoHomepage" src="/media/trialpagelogo.png" alt="Logo" width="920" height="450">
 </div>
 
 
@@ -80,10 +53,12 @@
 
     .logoHomepage {
         position: absolute;
-        left: 50px;
+        left: 147px;
     }
-
-    a:hover{
+    .dropdown-item:active{
+        background-color: black !important;
+    }
+    a.nav-link:hover{
         color: white !important; /* New text color on hover */
         text-shadow:
                 -1px -1px 0 #000,
@@ -91,7 +66,7 @@
                 -1px 1px 0 #000,
                 1px 1px 0 #000000;
     }
-    a{
+    a.nav-link{
         color: black !important;
     }
 
