0b77c4f0813240dbeb40d4c10b273485149915d2
Andy Rodriguez <577147@student.inholland.nl>
2023-12-06 13:34:58 +0100

    finished the hardcoded artist page

--- Webdev1_EndAssignment_IT2B_577147/app/views/artists/index.php
+++ Webdev1_EndAssignment_IT2B_577147/app/views/artists/index.php
@@ -11,72 +11,72 @@
     <ul class="dropdown-menu">
         <li><a class="dropdown-item" href="#">Action</a></li>
         <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <li><a class="dropdown-item" href="#">Something else</a></li>
     </ul>
 </div>
-<div class="album py-5 bg-body-tertiary">
+<div class="album py-5">
     <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
             <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Artist" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
+                <a class="button" href="#"><div class="card shadow-sm position-relative">
+                        <img class="img" width="100%" height="225" src="/media/artist1.png" role="img" focusable="false">
+                        <div class="overlay-text">Artist Name</div>
+                    <!--<div class="card-body">
                         <p class="card-text">Artist Name</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                        </div>
-                    </div>
-                </div>
+                    </div>-->
+                </div></a>
             </div>
             <div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Artist" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                        <p class="card-text">Artist Name</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                        </div>
-                    </div>
-                </div>
-            </div><div class="col">
-                <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Artist" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                        <p class="card-text">Artist Name</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                        </div>
-                    </div>
-                </div>
+                <a class="button" href="#"><div class="card shadow-sm position-relative">
+                        <img class="img" width="100%" height="225" src="/media/artist2.png" role="img" focusable="false">
+                        <div class="overlay-text">Artist Name</div>
+                        <!--<div class="card-body">
+                            <p class="card-text">Artist Name</p>
+                        </div>-->
+                    </div></a>
             </div>
             <div class="col">
-                <div class="card shadow-sm mt-3">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Artist" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                        <p class="card-text">Artist Name</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                        </div>
-                    </div>
-                </div>
+                <a class="button" href="#"><div class="card shadow-sm position-relative">
+                        <img class="img" width="100%" height="225" src="/media/artist1.png" role="img" focusable="false">
+                        <div class="overlay-text">Artist Name</div>
+                        <!--<div class="card-body">
+                            <p class="card-text">Artist Name</p>
+                        </div>-->
+                    </div></a>
             </div>
             <div class="col">
-                <div class="card shadow-sm mt-3">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Artist" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                        <p class="card-text">Artist Name</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                        </div>
-                    </div>
-                </div>
+                <a class="button" href="#"><div class="card shadow-sm position-relative">
+                        <img class="img" width="100%" height="225" src="/media/artist2.png" role="img" focusable="false">
+                        <div class="overlay-text">Artist Name</div>
+                        <!--<div class="card-body">
+                            <p class="card-text">Artist Name</p>
+                        </div>-->
+                    </div></a>
             </div>
             <div class="col">
-                <div class="card shadow-sm mt-3">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Artist" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                    <div class="card-body">
-                        <p class="card-text">Artist Name</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                        </div>
-                    </div>
-                </div>
+                <a class="button" href="#"><div class="card shadow-sm position-relative">
+                        <img class="img" width="100%" height="225" src="/media/artist1.png" role="img" focusable="false">
+                        <div class="overlay-text">Artist Name</div>
+                        <!--<div class="card-body">
+                            <p class="card-text">Artist Name</p>
+                        </div>-->
+                    </div></a>
             </div>
+            <div class="col">
+                <a class="button" href="#"><div class="card shadow-sm position-relative">
+                        <img class="img" width="100%" height="225" src="/media/artist2.png" role="img" focusable="false">
+                        <div class="overlay-text">Artist Name</div>
+                        <!--<div class="card-body">
+                            <p class="card-text">Artist Name</p>
+                        </div>-->
+                    </div></a>
+            </div>
+
+
+        </div>
+            </div>
+                </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
@@ -89,7 +89,14 @@
                 -1px 1px 0 #000,
                 1px 1px 0 #000000 !important;
     }
-
+    .dropdown-menu{
+        border-width: 3px !important;
+        border-radius: 0 !important;
+        border-color: black !important;
+    }
+    .dropdown-toggle{
+        border-color: purple;
+    }
     button{
         border-radius: 0 !important;
         border-color: black !important;
@@ -101,6 +108,23 @@
 
     .card {
         border-radius: 0 !important;
+    }
+    .overlay-text{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-weight: bold;
+        font-size: 30px;
+        opacity: 0;
+        transition: opacity 0.3s ease-in-out;
+    }
 
+    .card:hover .overlay-text{
+        opacity: 1;
+    }
+    .card img:hover{
+        filter: blur(3px);
     }
 </style>
