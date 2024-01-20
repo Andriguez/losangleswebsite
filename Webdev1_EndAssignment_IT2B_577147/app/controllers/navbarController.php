682bf5c84b294beb9554812b7e48049e499ee44e
Andy Rodriguez <577147@student.inholland.nl>
2024-01-20 16:46:52 +0100

    changed the logo and made it interactive

--- Webdev1_EndAssignment_IT2B_577147/app/controllers/navbarController.php
+++ Webdev1_EndAssignment_IT2B_577147/app/controllers/navbarController.php
@@ -16,6 +16,15 @@ class navbarController extends Controller
     }
     public function displayNavbar()
     {
+        $logoSrc = '/media/logos/logo-letters-bigger.png';
+        $parsedUrl = parse_url($_SERVER['REQUEST_URI']);
+        $requestedPath = $parsedUrl['path'];
+        $homepageUrls = ['/', '/index', 'index.php'];
+
+        if(in_array($requestedPath, $homepageUrls)){
+            $logoSrc = '/media/logos/losangles-text.png';
+        }
+
         if (isset($_SESSION['user_id'])){ $loggedUser = $this->userService->getUserById($_SESSION['user_id']); }
         require __DIR__ . '/../views/navbar.php';
     }
