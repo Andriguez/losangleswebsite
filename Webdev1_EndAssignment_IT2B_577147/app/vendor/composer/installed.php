48a053e1f0e840934f5ee99f1fa459ae33d2ee9f
Andy Rodriguez <577147@student.inholland.nl>
2024-07-04 04:17:53 +0200

    composer + autoload seemingly installed

---
+++ Webdev1_EndAssignment_IT2B_577147/app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'andyrodriguez/losanglesweb',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b84fe85bf24d3bec27fd5674eca0b11bb160b8e9',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'andyrodriguez/losanglesweb' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b84fe85bf24d3bec27fd5674eca0b11bb160b8e9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
