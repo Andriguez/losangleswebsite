689840ed9a66028096584c2e0b7ef0fac1efe67e
Andy Rodriguez <577147@student.inholland.nl>
2024-07-19 03:57:41 +0200

    trying to make it so the default port is the listened port

--- Webdev1_EndAssignment_IT2B_577147/nginx-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace the placeholder in the template with the actual PORT value
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-# Start Nginx
-nginx -g 'daemon off;'
