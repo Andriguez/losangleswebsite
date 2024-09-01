a68f5981c5c98064ba664b407d9c8da06df8664b
Andy Rodriguez <577147@student.inholland.nl>
2024-09-01 21:12:26 +0200

    even more height

--- Webdev1_EndAssignment_IT2B_577147/app/views/artists/detailpage.php
+++ Webdev1_EndAssignment_IT2B_577147/app/views/artists/detailpage.php
@@ -207,7 +207,7 @@
             color: white !important;
             display: flex;
             min-height: 280px;
-            max-height: 340px;
+            max-height: 600px;
 
             img {
                 height: 250px !important;
@@ -307,7 +307,7 @@
             color: white !important;
             display: flex;
             min-height: 280px;
-            max-height: 340px;
+            max-height: 600px;
 
             img {
                 height: 250px !important;
