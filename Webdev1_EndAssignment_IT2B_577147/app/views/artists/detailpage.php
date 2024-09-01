b20b9034aadfbe4332c49c1050c0697cbfadb298
Andy Rodriguez <577147@student.inholland.nl>
2024-09-01 21:50:43 +0200

    try to make the height even bigger and soundcloud smaller

--- Webdev1_EndAssignment_IT2B_577147/app/views/artists/detailpage.php
+++ Webdev1_EndAssignment_IT2B_577147/app/views/artists/detailpage.php
@@ -41,7 +41,7 @@
     </div>
     <div class="media-container">
         <?php if ($artist->getArtistContent()->getSoundcloudUrl() != '#'){?>
-            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="<?php echo $artist->getArtistContent()->getSoundcloudUrl() ?>"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
+            <iframe width="100%" height="156" scrolling="no" frameborder="no" allow="autoplay" src="<?php echo $artist->getArtistContent()->getSoundcloudUrl() ?>"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
         <?php } ?>
     </div>
 </div>
@@ -67,7 +67,7 @@
     <div class="media-container">
         <?php if ($artist->getArtistContent()->getSoundcloudUrl() != '#'){?>
             <?php if (isset($artist)) { ?>
-                <iframe width="100%" height="200" scrolling="no" frameborder="no" allow="autoplay" src="<?php echo $artist->getArtistContent()->getSoundcloudUrl() ?>"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
+                <iframe width="100%" height="180" scrolling="no" frameborder="no" allow="autoplay" src="<?php echo $artist->getArtistContent()->getSoundcloudUrl() ?>"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"></div>
             <?php } else { echo ''; } ?>
         <?php } ?>
     </div>
@@ -207,7 +207,7 @@
             color: white !important;
             display: flex;
             min-height: 280px;
-            max-height: 600px;
+            max-height: 700px;
 
             img {
                 height: 250px !important;
@@ -307,7 +307,7 @@
             color: white !important;
             display: flex;
             min-height: 280px;
-            max-height: 600px;
+            max-height: 800px;
 
             img {
                 height: 250px !important;
