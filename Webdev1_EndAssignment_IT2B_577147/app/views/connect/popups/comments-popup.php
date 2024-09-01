980c8a0aeec019404f6ed77522ba44e4353ed9bf
Andy Rodriguez <577147@student.inholland.nl>
2024-09-02 00:21:56 +0200

    changing font in comments

--- Webdev1_EndAssignment_IT2B_577147/app/views/connect/popups/comments-popup.php
+++ Webdev1_EndAssignment_IT2B_577147/app/views/connect/popups/comments-popup.php
@@ -16,17 +16,17 @@
         </div>
     </div>
     <?php if (isset($comments)){ foreach ($comments as $comment){?>
-    <div id="<?php echo $comment->getId()?>" class="comment">
-        <div class="name-section">
-            <span class="poster-name"><?php echo $comment->getUser()->getFullName()?></span>
+        <div id="<?php echo $comment->getId()?>" class="comment">
+            <div class="name-section">
+                <span class="poster-name"><?php echo $comment->getUser()->getFullName()?></span>
+            </div>
+            <div class="content-section">
+                <span><?php echo $comment->getContentText()?></span>
+            </div>
+            <div class="btns-section">
+                <label>commented on<cite title="time stamp"><?php echo ' '.$comment->getPostedAt()->format('D d/m/y').' at '.$comment->getPostedAt()->format('H:i')?></cite></label>
+            </div>
         </div>
-        <div class="content-section">
-            <span><?php echo $comment->getContentText()?></span>
-        </div>
-        <div class="btns-section">
-            <label>commented on<cite title="time stamp"><?php echo ' '.$comment->getPostedAt()->format('D d/m/y').' at '.$comment->getPostedAt()->format('H:i')?></cite></label>
-        </div>
-    </div>
     <?php }} ?>
 
     <div class="comment-submit">
@@ -36,6 +36,8 @@
 </div>
 </body>
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&display=swap');
+
     #comments-section{
         overflow-y: scroll;
         background-color: black;
@@ -57,92 +59,104 @@
                     }
                 }
                 #parent-post-title{
-                    font-family: "Agency FB" !important;
-                    font-size: 24px;
-                    font-weight: bold;
+                    font-family: "Agdasima", sans-serif;
+                    font-weight: 700;
+                    font-style: normal;
+                    font-size: 26px;
                 }
                 #parent-post-content{
-                    font-family: "Agency FB" !important;
-                    font-size: 20px;
+                    font-family: "Agdasima", sans-serif;
+                    font-weight: 400;
+                    font-style: normal;
+                    font-size: 22px;
                     max-height: 100px;
                     overflow-y: auto;
                 }
             }
         }
 
-    .comment{
-        border: white solid 2px;
-        color: black;
-        background-color: white;
-        padding: 5px;
-        margin-bottom: 5px;
-        font-family: "Agency FB";
-        font-size: 20px;
+        .comment{
+            border: white solid 2px;
+            color: black;
+            background-color: white;
+            padding: 5px;
+            margin-bottom: 5px;
+            font-family: "Agdasima", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+            font-size: 22px;
 
-        .btns-section{
-            display: flex;
-            justify-content: flex-end;
+            .btns-section{
+                display: flex;
+                justify-content: flex-end;
 
-            a{
-                margin: 5px;
-                font-weight: bold;
-            }
-            label{
-                font-size: 14px;
+                a{
+                    margin: 5px;
+                    font-weight: bold;
+                }
+                label{
+                    font-size: 14px;
+                }
             }
         }
-    }
 
-            .name-section{
-                margin-top: 5px;
-                margin-bottom: 2px;
-            }
+        .name-section{
+            margin-top: 5px;
+            margin-bottom: 2px;
+        }
 
-            a{
-                color: black;
-                font-family: "Agency FB";
-                font-size: 18px;
-            }
-            a:hover{
-                background-color: white;
-                filter: invert(100%);
-            }
+        a{
+            color: black;
+            font-family: "Agdasima", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+            font-size: 20px;
+        }
+        a:hover{
+            background-color: white;
+            filter: invert(100%);
+        }
 
-            .poster-name{
-                font-family: angles !important;
-                font-size: 11px;
-                text-transform: uppercase;
-                font-weight: bold;
-            }
+        .poster-name{
+            font-family: "Agdasima", sans-serif;
+            font-weight: 700;
+            font-style: normal;
+            font-size: 13px;
+            text-transform: uppercase;
+        }
 
-            .comment-submit{
-                width: 100%;
-                position: sticky;
-                bottom: 0;
-                margin: 0;
-                height: 30px;
-                background-color: white;
+        .comment-submit{
+            width: 100%;
+            position: sticky;
+            bottom: 0;
+            margin: 0;
+            height: 30px;
+            background-color: white;
 
 
             input{
                 width: 85%;
                 position: relative;
                 height: 30px;
-                font-family: "Agency FB";
-                font-size: 18px;
+                font-family: "Agdasima", sans-serif;
+                font-weight: 400;
+                font-style: normal;
+                font-size: 20px;
                 border: 0 solid white;
             }
-                input:focus{
-                    outline: none;
-                }
+            input:focus{
+                outline: none;
+            }
             button {
                 position: absolute;
                 right: 0;
-                font-family: "Agency FB";
-                font-size: 18px;
-                font-weight: bold;
+                font-family: "Agdasima", sans-serif;
+                font-weight: 700;
+                font-style: normal;
+                font-size: 20px;
                 border-width: 3px;
                 background-color: white;
+                color: black;
             }
             button:hover{
                 background-color: black;
